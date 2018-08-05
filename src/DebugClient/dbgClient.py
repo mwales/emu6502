fd8a3c6348f67e9ad6eaad5f14174cb3828b097d
Michael Wales <mwales3@gmail.com>
2018-08-05 19:54:39 -0400

    Added connect command to debugger client, and fixed whitespace issues

--- src/DebugClient/dbgClient.py
+++ src/DebugClient/dbgClient.py
@@ -27,46 +27,48 @@ def hexDump(dataAddr, data, numBytes):
     asciiLine = ""
     print("Memory dump debug: dumpStart = {}, dumpEnd = {}".format(prettyhex(dumpLineStart, 16), prettyhex(dumpLineEnd, 16)))
     for curAddr in range(dumpLineStart, dumpLineEnd + 1):
-       # Print out the address
-       if ( (curAddr & 0xfff0) == curAddr ):
-           # This is the beginning of a line, print address
-           retVal += prettyhex(curAddr, 16)
-	   retVal += "    "
-	   #print("Starting new line @ {}".format(prettyhex(curAddr, 16)))
-	   asciiLine = "    |"
-       
-       # Print out the data bytes
-       if ( (curAddr >= dataAddr) and (curAddr < dataEnd) ):
-           # print("Going to add char at address {}, index {} in data".format(prettyhex(curAddr, 16), curAddr - dataAddr))
-           
-	   ordVal = ord(data[curAddr - dataAddr])
-	   retVal += prettyhex(ordVal, 8)
-	   
-	   # add the character to teh ascii buf at the end of the line
-           if ( (ordVal >= 0x20) and (ordVal <= 0x7e) ):
-	       asciiLine += chr(ordVal)
-	   else:
-	       asciiLine += "."
-       else:
-           print("Address {} is a blank in the md".format(prettyhex(curAddr, 16)))
-           retVal += "  "
-	   asciiLine += " "
+        # Print out the address
+        if ( (curAddr & 0xfff0) == curAddr ):
+            # This is the beginning of a line, print address
+            retVal += prettyhex(curAddr, 16)
+            retVal += "    "
+            #print("Starting new line @ {}".format(prettyhex(curAddr, 16)))
+            asciiLine = "    |"
 
-       retVal += " "
+        # Print out the data bytes
+        if ( (curAddr >= dataAddr) and (curAddr < dataEnd) ):
+            # print("Going to add char at address {}, index {} in data".format(prettyhex(curAddr, 16), curAddr - dataAddr))
 
-       # Print a space after the 8th byte for easy reading
-       if ( (curAddr & 0xf) == 0x7):
-           retVal += " "
+            ordVal = ord(data[curAddr - dataAddr])
+            retVal += prettyhex(ordVal, 8)
 
-       #Print the line ending on the last character of the line
-       if ( (curAddr & 0xf) == 0xf):
-           asciiLine += "|"
+            # add the character to teh ascii buf at the end of the line
+            if ( (ordVal >= 0x20) and (ordVal <= 0x7e) ):
+                asciiLine += chr(ordVal)
+            else:
+                asciiLine += "."
+        else:
+            print("Address {} is a blank in the md".format(prettyhex(curAddr, 16)))
+            retVal += "  "
+            asciiLine += " "
 
-	   retVal += asciiLine
+        retVal += " "
 
-           retVal += "\n"
+        # Print a space after the 8th byte for easy reading
+        if ( (curAddr & 0xf) == 0x7):
+            retVal += " "
+
+        #Print the line ending on the last character of the line
+        if ( (curAddr & 0xf) == 0xf):
+            asciiLine += "|"
+
+            retVal += asciiLine
+
+            retVal += "\n"
 
     return retVal
+
+
 
 
 def main(argv):
@@ -96,7 +98,7 @@ def main(argv):
 
 class DbgClient(cmd2.Cmd):
 
-    def __init__(self, s):
+    def __init__(self, s = None):
         cmd2.Cmd.__init__(self)
 
         self.intro = "Debugger Client for 6502 Emulator"
@@ -122,11 +124,29 @@ class DbgClient(cmd2.Cmd):
             print("No data in frame to receive, return empty string")
             return ""
 
-    def do_stuff(self, args):
+    def do_connect(self, argList):
         """
-        This is the awesome doucmentation for the stuff command!
+        Connects to a 6502 emulator
+
+        Usage:  connect ip port
         """
-        print("Hey, look at me: {}".format(args))
+        args = argList.split()
+
+        if (len(args) != 2):
+            print("Usage: connect ip port")
+            return
+
+        ipAddress = args[0]
+        portNum = int(args[1])
+
+        print("Attempting to connect to debug server {}:{}".format(ipAddress, portNum))
+
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.connect((ipAddress, portNum))
+        except IOError as e:
+            print("Failed to connect to the debugger server: {}".format(e.message))
+            return
 
     def do_about(self, args):
         """
@@ -136,7 +156,7 @@ class DbgClient(cmd2.Cmd):
 
         emuVersion = self.receiveMessage()
         print("Emulator: {}".format(emuVersion))
-	print("Client: Python 6502 Debug Client v0.0")
+        print("Client: Python 6502 Debug Client v0.0")
 
     def do_shutdown(self, args):
         """
@@ -146,147 +166,148 @@ class DbgClient(cmd2.Cmd):
 
     def do_disass(self, args):
         """
-	Disassembles code
+        Disassembles code
 
-	disass [address] [numInstructinos]
+        disass [address] [numInstructinos]
 
-	Address is always interpreted as hexadecimal
-	"""
+        Address is always interpreted as hexadecimal
+        """
 
         argList = args.split()
         
-	if (len(argList) == 0):
-	    flags = 0     # no address, no num instructions
-	    address = 0
-	    num = 0
-	elif(len(argList) == 1):
-	    flags = 1
-	    address = int(argList[0], 16)
-	    num = 0
-	else:
-	    flags = 3
-	    address = int(argList[0], 16)
+        if (len(argList) == 0):
+            flags = 0     # no address, no num instructions
+            address = 0
+            num = 0
+        elif(len(argList) == 1):
+            flags = 1
+            address = int(argList[0], 16)
+            num = 0
+        else:
+            flags = 3
+            address = int(argList[0], 16)
             num = int(argList[1])
 
-	print("Sending disassemble command(flags = {}, address = {}, numInstructions = {}".format(flags, hex(address), num))
+        print("Sending disassemble command(flags = {}, address = {}, numInstructions = {}".format(flags, hex(address), num))
 
-	self.sendHeader(3, 5)
-	msgData = struct.pack("!BHH", flags, address, num)
-	self.s.send(msgData)
+        self.sendHeader(3, 5)
+        msgData = struct.pack("!BHH", flags, address, num)
+        self.s.send(msgData)
 
-	rsp = self.receiveMessage()
-	print("Listing:\n{}".format(rsp))
+        rsp = self.receiveMessage()
+        print("Listing:\n{}".format(rsp))
+
 
     def do_regs(self, args):
         """
-	Dumps the registers
-	"""
+        Dumps the registers
+        """
 
         self.sendHeader(4,0)
-	self.receiveRegisterData()
+        self.receiveRegisterData()
 
     def do_step(self, argList):
         """
-	Step the debugger some finite number of instructions.
-	
-	Default is 1 instruction, or you can provide number of instructions to step
-	"""
-	args = argList.split()
-	if (len(args) == 0):
-	   stepCount = 1
-	else:
-	   stepCount = int(args[0], 16)
+        Step the debugger some finite number of instructions.
 
-	print("Stepping {} (0x{}) steps".format(stepCount, prettyhex(stepCount, 16)))
-	
-	self.sendHeader(5,2)
+        Default is 1 instruction, or you can provide number of instructions to step
+        """
+        args = argList.split()
+        if (len(args) == 0):
+           stepCount = 1
+        else:
+           stepCount = int(args[0], 16)
+
+        print("Stepping {} (0x{}) steps".format(stepCount, prettyhex(stepCount, 16)))
+
+        self.sendHeader(5,2)
 
         msgBody = struct.pack("!H", stepCount)
-	self.s.send(msgBody)
+        self.s.send(msgBody)
 
-	self.receiveRegisterData()
+        self.receiveRegisterData()
 
     def do_halt(self, args):
         """
-	Halt the emulator so we can debug
-	"""
+        Halt the emulator so we can debug
+        """
         self.sendHeader(6,0)
-	self.receiveRegisterData()
+        self.receiveRegisterData()
 
     def do_continue(self, args):
         """
-	Continue emulator execution
-	"""
+        Continue emulator execution
+        """
 
         self.sendHeader(7,0)
         self.receiveRegisterData()
    
     def receiveRegisterData(self):
-	rspData = self.receiveMessage()
+        rspData = self.receiveMessage()
 
-	# Going to return X, Y, Accum, StackPointer, PC, statusReg, Padding, NumClocksHigh, NumClocksLow
-	   
+        # Going to return X, Y, Accum, StackPointer, PC, statusReg, Padding, NumClocksHigh, NumClocksLow
+
         (x, y, accum, sp, pc, status, padding, numClkHigh, numClkLow) = struct.unpack("!BBBBHBBLL", rspData)
 
-	print(" X={}   Y={}    A={}".format(prettyhex(x, 8), prettyhex(y, 8), prettyhex(accum, 8)))
-	print("SP={}  PC={}".format(prettyhex(sp, 8), prettyhex(pc, 16)))
+        print(" X={}   Y={}    A={}".format(prettyhex(x, 8), prettyhex(y, 8), prettyhex(accum, 8)))
+        print("SP={}  PC={}".format(prettyhex(sp, 8), prettyhex(pc, 16)))
 
         statusFlags = []
-	if (status & 0x01):
-	    statusFlags.append("FLG_CARY")
-	if (status & 0x02):
-	    statusFlags.append("FLG_ZERO")
-	if (status & 0x04):
-	    statusFlags.append("FLG_INTD")
-	if (status & 0x08):
-	    statusFlags.append("FLG_DECI")
-	if (status & 0x10):
-	    statsuFlags.append("FLG_BKPT")
-	if (status & 0x40):
-	    statusFlags.append("FLG_OVFL")
+        if (status & 0x01):
+            statusFlags.append("FLG_CARY")
+        if (status & 0x02):
+            statusFlags.append("FLG_ZERO")
+        if (status & 0x04):
+            statusFlags.append("FLG_INTD")
+        if (status & 0x08):
+            statusFlags.append("FLG_DECI")
+        if (status & 0x10):
+            statsuFlags.append("FLG_BKPT")
+        if (status & 0x40):
+            statusFlags.append("FLG_OVFL")
         if (status & 0x80):
-	    statusFlags.append("FLG_NEG")
+            statusFlags.append("FLG_NEG")
 
-	print("SR={} = {}".format(prettyhex(status, 8), " | ".join(statusFlags)))
+        print("SR={} = {}".format(prettyhex(status, 8), " | ".join(statusFlags)))
 
         print("Num Clocks = {}{}".format(prettyhex(numClkHigh, 32), prettyhex(numClkLow, 32)))
 
     def do_md(self, argstr):
         """
-	Memory dump.
+        Memory dump.
 
-	md address [numbytes]
+        md address [numbytes]
 
-	Defaults to dumping 0x100 bytes
-	"""
+        Defaults to dumping 0x100 bytes
+        """
 
         args = argstr.split()
         
-	if (len(args) < 1):
-	    print("You need to specify an address")
-	    return
+        if (len(args) < 1):
+            print("You need to specify an address")
+            return
 
-	address = int(args[0], 16)
-	numBytes = 0x100
-	if (len(args) >= 2):
-	    numBytes = int(args[1], 16)
+        address = int(args[0], 16)
+        numBytes = 0x100
+        if (len(args) >= 2):
+            numBytes = int(args[1], 16)
 
-	print("Requesting a memory dump of {} bytes at address {}".format(prettyhex(numBytes, 16), prettyhex(address, 16)))
+        print("Requesting a memory dump of {} bytes at address {}".format(prettyhex(numBytes, 16), prettyhex(address, 16)))
 
-	self.sendHeader(8,4)
+        self.sendHeader(8,4)
 
-	msgData = struct.pack("!HH", address, numBytes)
-	self.s.send(msgData)
+        msgData = struct.pack("!HH", address, numBytes)
+        self.s.send(msgData)
 
         rsp = self.receiveMessage()
 
-	(addrRx, bytesRx) = struct.unpack("!HH", rsp[0:4])
-	print("Received {} bytes from address {}".format(prettyhex(bytesRx, 16), prettyhex(addrRx, 16)))
+        (addrRx, bytesRx) = struct.unpack("!HH", rsp[0:4])
+        print("Received {} bytes from address {}".format(prettyhex(bytesRx, 16), prettyhex(addrRx, 16)))
 
         dataBuf = rsp[4:]
-	if (len(dataBuf) < bytesRx):
-	    print("Didn't receive the number of bytes indicated in memory dump")
-	    return
+        if (len(dataBuf) < bytesRx):
+            print("Didn't receive the number of bytes indicated in memory dump")
+            return
 
         hd = hexDump(addrRx, dataBuf, bytesRx)
         print(hd)
